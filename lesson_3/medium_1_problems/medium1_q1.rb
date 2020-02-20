6ea4cc6f4da2b50064da9edc8d79c20b842d338a
Christin Hebron <puilunchristin@gmail.com>
2020-02-20 21:45:59 +0000

    Added medium1 problems

---
+++ lesson_3/medium_1_problems/medium1_q1.rb
@@ -0,0 +1,8 @@
+=begin
+For this practice problem,
+write a one-line program that
+creates the following output 10 times,
+with the subsequent line indented 1 space to the right:
+=end
+
+10.times { |number| puts (" " * number) + "The Flintstones Rock!"}
---
+++ lesson_3/medium_1_problems/medium1_q2.rb
@@ -0,0 +1,9 @@
+#The result of the following statement will be an error:
+
+puts "the value of 40 + 2 is " + (40 + 2)
+
+(40+2).to_s
+
+or
+
+puts "the value of 40 + 2 is #{40 + 2}"
