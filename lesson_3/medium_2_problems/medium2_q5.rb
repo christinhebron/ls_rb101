451149db9664c1e36d0f93823629ccb39b5d0a88
Christin Hebron <puilunchristin@gmail.com>
2020-02-21 21:36:20 +0000

    Added lesson problems

---
+++ lesson_3/medium_2_problems/medium2_q5.rb
@@ -0,0 +1,28 @@
+#How can we refactor this practice problem to make the result easier to predict and easier for the next programmer to maintain?
+
+def tricky_method(a_string_param, an_array_param)
+  a_string_param += "rutabaga"
+  an_array_param << "rutabaga"
+end
+
+my_string = "pumpkins"
+my_array = ["pumpkins"]
+tricky_method(my_string, my_array)
+
+puts "My string looks like this now: #{my_string}"
+puts "My array looks like this now: #{my_array}"
+
+
+def not_so_tricky_method(a_string_param, an_array_param)
+  a_string_param += "rutabaga"
+  an_array_param += ["rutabaga"]
+
+  return a_string_param, an_array_param
+end
+
+my_string = "pumpkins"
+my_array = ["pumpkins"]
+my_string, my_array = not_so_tricky_method(my_string, my_array)
+
+puts "My string looks like this now: #{my_string}"
+puts "My array looks like this now: #{my_array}"
